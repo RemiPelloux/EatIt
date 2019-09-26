c39711d1e6c7f18dcf1ff180fa4c5c8066122305
tkmremi <tkmremi@gmail.com>
2019-09-26 14:27:08 +0200

    Modif : index.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>EatIt</title>
     <?php include "packages/package_required.php" ?>
 </head>
+
 <body>
 
 <?php echo "it works" ?>
 
 </body>
-</html>
+
+</html>l
