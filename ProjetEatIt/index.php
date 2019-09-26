aba6fe8b3764c6293d58a15e1d4f13c3cbbe0dc4
Remi <tkmremi@gmail.com>
2019-09-26 11:19:01 +0200

    Base projet

---
+++ ProjetEatIt/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>EatIt</title>
+    <?php include "packages/package_required.php" ?>
+</head>
+<body>
+
+<?php echo "it works" ?>
+
+</body>
+</html>
